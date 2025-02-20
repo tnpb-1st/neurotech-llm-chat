8f830f75424dbb316a518f2143c00375c5e90d61
Tomás Nascimento Pimentel Barros <samotbnp@gmail.com>
2025-02-19 21:28:06 -0300

    created a class for the streamlit app and added it to a main file

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.streamlit_app.app import DataChatApp
+
+
+if __name__ == "__main__":
+    app = DataChatApp()
+    app.run()
--- src/streamlit_app/app.py
+++ src/streamlit_app/app.py
@@ -6,66 +6,71 @@ from src.llms.agents.insight_generator import InsightsGenerator
 from config.constants import TABLE_INFO
 
 
-# Cria um DataFrame com as informações da tabela
-SCHEMA_DF = pd.DataFrame(TABLE_INFO)
-QUERY_VALIDATOR_AGENT = QueryValidator()
-SQL_EXECUTOR_AGENT = SQLExecutor()
-INSIGHTS_GENERATOR_AGENT = InsightsGenerator()
+class DataChatApp:
+    def __init__(self):
+        # Cria um DataFrame com as informações do esquema da tabela
+        self.schema_df = pd.DataFrame(TABLE_INFO)
+        # Inicializa os agentes
+        self.query_validator_agent = QueryValidator()
+        self.sql_executor_agent = SQLExecutor()
+        self.insights_generator_agent = InsightsGenerator()
+
+    def run(self):
+        st.title("Chatbot de Análise de Dados (SQL)")
+
+        # Botão para reiniciar o chat
+        if st.button("Novo Chat", type="primary"):
+            st.rerun()
+
+        initial_message = (
+            "Olá. Eu sou o Neurochat, estou aqui para ser seu assistente de análise de dados. Confira abaixo as "
+            "informações do Banco de Dados. Você pode me fazer perguntas ou consultas sobre esses dados e eu irei te "
+            "ajudar a responder. Vamos lá!"
+        )
+
+        with st.chat_message("assistant"):
+            st.write(initial_message)
+            st.dataframe(self.schema_df, hide_index=True)
+
+        # Captura o input do usuário
+        if prompt := st.chat_input("Escreva seu comando para o neurochat: "):
+            with st.chat_message("user"):
+                st.write(prompt)
+
+            # Validação do input
+            st.write("Validando input...🔍")
+            validate_status, reason = self.query_validator_agent.validate_query(prompt)
+            is_valid = validate_status == "valid"
+
+            with st.chat_message("assistant"):
+                if is_valid:
+                    st.write("Input válido✅")
+                    st.write("Processando Query SQL...🔄")
+                    query_result = self.sql_executor_agent.run(prompt)
+                    if "error" in query_result:
+                        st.warning(
+                            f"Erro ao processar a query SQL: {query_result['error']} "
+                            "Por favor, tente novamente!"
+                        )
+                    else:
+                        st.write("Query SQL processada com sucesso✅")
+                        st.write("Exibindo os resultados...📊")
+                        st.code(query_result["sql_query"], language="sql")
+                        st.dataframe(query_result["data"], hide_index=True)
+                        if "data" in query_result and len(query_result["data"]) > 0:
+                            # Adiciona o input original no dicionário para gerar os insights
+                            insight_dir = query_result
+                            insight_dir["input"] = prompt
+                            st.write("Gerando insights...📊")
+                            insights = self.insights_generator_agent.generate_insights(insight_dir)
+                            st.write(insights)
+                else:
+                    st.warning(
+                        f"Requisição inválida‼️\nSeu input foi considerado inválido por: {reason} "
+                        "Por favor, tente novamente!"
+                    )
 
 
-# Streamlit
-st.title("Chatbot de Análise de Dados (SQL)")
-col1 = st.columns([0.2])[0]
-
-# with col1:
-if st.button("Novo Chat", type="primary"):
-    st.rerun()
-
-
-initial_message = (
-    "Olá . Eu sou o Neurochat, estou aqui para ser seu assistente de análise de dados. Confira abaixo as "
-    "informações do Banco de Dados. Você pode me fazer perguntas ou consultas sobre esses dados que eu ir"
-    "ei te ajudar a responder. Vamos lá!"
-)
-
-with st.chat_message("assistant"):
-    st.write(initial_message)
-    st.dataframe(SCHEMA_DF, hide_index=True)
-
-if prompt := st.chat_input("Escreva seu comando para o neurochat: "):
-    with st.chat_message("user"):
-        st.write(prompt)
-
-    # VALIDATE INPUT
-    st.write("Validando input...🔍")
-    validate_status, reason = QUERY_VALIDATOR_AGENT.validate_query(prompt)
-    is_valid = validate_status == "valid"
-    with st.chat_message("assistant"):
-        if is_valid:
-            st.write("Input valido✅")
-            st.write("Processando Query SQL...🔄")
-            # final_response = {"sql_query": sql_query, "data": query_result["data"]}
-            # final_response = {"error": ...}
-            query_result = SQL_EXECUTOR_AGENT.run(prompt)
-            if "error" in query_result:
-                st.warning(
-                    f"Erro ao processar a query SQL: {query_result['error']}"
-                    "Por favor, tente novamente!"
-                )
-            else:
-                st.write("Query SQL processada com sucesso✅")
-                st.write("Exibindo os resultados...📊")
-                st.code(query_result["sql_query"], language="sql")
-                st.dataframe(query_result["data"], hide_index=True)
-                if "data" in query_result and len(query_result["data"]) > 0:
-                    insight_dir = query_result
-                    insight_dir["input"] = prompt
-                    st.write("Gerando insights...📊")
-                    insights = INSIGHTS_GENERATOR_AGENT.generate_insights(insight_dir)
-                    st.write(insights)
-
-        else:
-            st.warning(
-                f"Requisição inválida‼️\nSeu input foi considerado ivalid por: {reason}"
-                "Por favor, tente novamente!"
-            )
+if __name__ == "__main__":
+    app = DataChatApp()
+    app.run()
